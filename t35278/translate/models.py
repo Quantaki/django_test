fa7da3bb746a1b1ee2396ffa3dc7c7f568662a24
quantaki <karimbentorcha@gmail.com>
2024-03-23 10:13:45 +0100

    translate choiceField test

--- t35278/translate/models.py
+++ t35278/translate/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+FIELD_CHOICES = [
+    ("my_choice_one", _("My choice one")),
+    ("last_choice", _("Last choice")),
+]
+class Test(models.Model) :
+
+    field = models.CharField(max_length=250, choices=FIELD_CHOICES )
--- t35278/translate/views.py
+++ t35278/translate/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.translation import gettext as _
 from django.utils.translation import activate # if you to force translate to another language..
+
+from translate.models import Test
 # Create your views here.
 
 def home_view(request) :
 
-    lang_pref = request.LANGUAGE_CODE
-    trans_text = _("Hello world !")
-
-    return HttpResponse(f"Text should be translated to {lang_pref} : {trans_text}")
+    context = {
+        "data" : Test.objects.all(),
+    }
+    return render(request, "translate/home.html", context=context)
 
 
